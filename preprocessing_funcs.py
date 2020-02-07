be9bf28b068b33ca49aa92c2b58e55a4be438ce9
Vrinda Gupta <vrinda2592@gmail.com>
2020-02-07 13:26:07 +0530

    Update preprocessing_funcs.py

--- preprocessing_funcs.py
+++ preprocessing_funcs.py
@@ -14,3 +14,28 @@ def missing_values(data):
     print (df[~(df['Total'] == 0)]) 
     
     return df
+
+def get_cols(string_list, data):
+    cols_all = []
+    for string in string_list:
+        cols = [col for col in data.columns if string in col]
+        print(cols)
+        cols_all.extend(cols)
+    return(cols)
+
+def create_groups(row):
+    if row in [0,1,2,3,4]:
+        return "Low"
+    elif row in [5,6,7]:
+        return "Med"
+    elif row in [8,9,10]:
+        return "High"
+    else:
+        return "others"
+
+def df_empty(columns, dtypes, index=None):
+    assert len(columns)==len(dtypes)
+    df = pd.DataFrame(index=index)
+    for c,d in zip(columns, dtypes):
+        df[c] = pd.Series(dtype=d)
+    return df
