47faa2d1eea606a487d777eedcf94b4d187a558b
Vrinda Gupta <vrinda2592@gmail.com>
2020-02-29 09:00:47 +0530

    Update data_reshaping.py

--- data_reshaping.py
+++ data_reshaping.py
@@ -1,5 +1,6 @@
-#Borrowed from stackoverflow
+#######################################Borrowed from stackoverflow##############################################
 
+################ VlookUp values of one column based on another dataframe 
 # Create sample DataFrames (data = delivery information, bands = lookup volume band, tariff = lookup cost by band):
 data = pd.DataFrame(columns = ['Customer', 'Zip', 'Volume'], 
                     data = [['A', 'RG', 0.7], ['B', 'KT', 1.3], ['C', 'NN', 1.8], ['D', 'PO', 2.4]])
@@ -14,3 +15,16 @@ data_banded = pd.merge_asof(data, bands, on = 'Volume', direction = 'forward')
 
 # Lookup the cost from the tariff table and apply to a new column called 'Cost' in the delivery data:
 data_banded['Cost'] = tariff.set_index('Zip').lookup(data_banded['Zip'], data_banded['Band'])
+
+
+################ Reanme Columns based on another #################
+df = df.rename(columns=dict(zip(feature_lib['old_val'],
+                             feature_lib['new_val'])))
+
+################ Comning multiple features #####################
+df = pd.DataFrame([['001', 'a', 1, 10, 100, 1000], ['002', 'b', 2, 20, 200, 2000]], columns=['id', 'name', 'c1', 'c2', 'c2', 'c3'], index=list('AB'))
+df = df.set_index(['id', 'name'])
+df = df.groupby(by=df.columns, axis=1).mean()
+df = df.reset_index()
+
+df
